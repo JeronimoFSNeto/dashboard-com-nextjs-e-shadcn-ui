5af8f45cf0fbf9007b0892744e4520c21073dcf2
jeronimofsneto <jeronimo@ufdpar.edu.br>
2024-08-19 17:33:04 -0300

    Projeto finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,89 @@
-import Image from "next/image";
+import { Sidebar } from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { BadgeDollarSign, DollarSign, Package, Users } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { ChartOverview } from "@/components/chart";
+import { Sales } from "@/components/sales";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Next.js + Shadcn UI</h1>
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Total vendas
+              </CardTitle>
+              <DollarSign className="ml-auto h-4 w-4" />
+            </div>
+
+            <CardDescription>Total de vendas em 90 dias</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">R$45.368</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Novos clientes
+              </CardTitle>
+              <Users className="ml-auto h-4 w-4" />
+            </div>
+
+            <CardDescription>Novos clientes em 30 dias</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">230</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Pedidos hoje
+              </CardTitle>
+              <Package className="ml-auto h-4 w-4" />
+            </div>
+
+            <CardDescription>Total de pedidos hoje</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">60</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Total pedidos
+              </CardTitle>
+              <BadgeDollarSign className="ml-auto h-4 w-4" />
+            </div>
+
+            <CardDescription>Total de pedidos em 30 dias</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">2300</p>
+          </CardContent>
+        </Card>
+      </section>
+      <section className="mt-4 flex flex-col md:flex-row gap-4">
+        <ChartOverview />
+        <Sales />
+      </section>
     </main>
   );
 }
